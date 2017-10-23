c1d44439eee3b6abd4bff7a6d379b4efac770ec7
Niko Mix <mix.niko@outlook.com>
2017-10-23 22:14:16 +0200

    First UDP Package

--- MixMedia.Upnp/MixMedia.Upnp/MixMedia.Upnp/DiscoveryService.cs
+++ MixMedia.Upnp/MixMedia.Upnp/MixMedia.Upnp/DiscoveryService.cs
@@ -19,18 +19,44 @@ namespace MixMedia.Upnp
         {
             try
             {
+               
                 var responses = new List<string>();
                 using (var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
                 {
                     udpSocket.ExclusiveAddressUse = true;
-                    
+
+
+                    udpSocket.IOControl(-1744830452, new[] {Convert.ToByte(false)}, null);
+
                     udpSocket.Bind(new IPEndPoint(localAddress, 0));
 
                     var multicastEndpoint = new IPEndPoint(IPAddress.Parse("239.255.255.250"), 1900);
+                    var req = "M-SEARCH * HTTP/1.1\r\n" +
+                              $"HOST: 239.255.255.250\r\n" +
+                              $"ST: *\r\n" +
+                              "MAN: \"ssdp:discover\"\r\n" +
+                              "MX: 10\r\n\r\n";
+                    var data = new ArraySegment<byte>(Encoding.UTF8.GetBytes(req));
+                    for (int i = 0; i < 3; i++)
+                    {
+                        await udpSocket.SendToAsync(data, SocketFlags.None, multicastEndpoint);
+                    }
 
-                    udpSocket.BeginReceive(new List<ArraySegment<byte>>(), SocketFlags.None, Callback, null);
+                    byte[] buffer = new byte[4096];
+                    int iRx = udpSocket.Receive(buffer);
+                    char[] chars = new char[iRx];
+
+                    System.Text.Decoder d = System.Text.Encoding.UTF8.GetDecoder();
+                    int charLen = d.GetChars(buffer, 0, iRx, chars, 0);
+                    System.String recv = new System.String(chars);
+
+                  Console.WriteLine(recv);
+
+        
+                    
+                    //var result = await udpSocket.ReceiveAsync(new ArraySegment<byte>(new byte[4096]), SocketFlags.None);
                     //udpSocket.EndReceive()
-
+      
 
                 }
             }
@@ -41,9 +67,51 @@ namespace MixMedia.Upnp
             await new Task(() => {});
         }
 
+        public class StateObject
+        {
+            // Client socket.
+            public Socket workSocket = null;
+            // Size of receive buffer.
+            public const int BufferSize = 256;
+            // Receive buffer.
+            public byte[] buffer = new byte[BufferSize];
+            // Received data string.
+            public StringBuilder sb = new StringBuilder();
+        }
+
         private static void Callback(IAsyncResult ar)
         {
-            throw new NotImplementedException();
+            try
+            {
+                // Retrieve the state object and the client socket 
+                // from the asynchronous state object.
+                StateObject state = (StateObject)ar.AsyncState;
+                Socket client = state.workSocket;
+                // Read data from the remote device.
+                int bytesRead = client.EndReceive(ar);
+                if (bytesRead > 0)
+                {
+                    // There might be more data, so store the data received so far.
+                    state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+                    //  Get the rest of the data.
+                    client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+                        new AsyncCallback(Callback), state);
+                }
+                else
+                {
+                    // All the data has arrived; put it in response.
+                    if (state.sb.Length > 1)
+                    {
+                        state.sb.ToString();
+                    }
+                    // Signal that all bytes have been received.
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
--- MixMedia.Upnp/Tests/MixMedia.Upnp.Tests/DiscoveryUnitTests.cs
+++ MixMedia.Upnp/Tests/MixMedia.Upnp.Tests/DiscoveryUnitTests.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace MixMedia.Upnp.Tests
@@ -6,9 +10,17 @@ namespace MixMedia.Upnp.Tests
     public class DiscoveryUnitTests
     {
         [Fact]
-        public void Discovery()
+        public async Task Discovery()
         {
-           Upnp.Discovery.Search()
+            var addresses = NetworkInterface.GetAllNetworkInterfaces().Where(address => address.OperationalStatus == OperationalStatus.Up);
+            foreach (var address in addresses)
+            {
+                foreach (var uniAddress in address.GetIPProperties().UnicastAddresses.Where(add => add.Address.AddressFamily == AddressFamily.InterNetwork))
+                {
+                 
+                    await DiscoveryService.Search(uniAddress.Address.MapToIPv4());
+                }
+            }
         }
     }
 }
